ecb95b60ec85c4d9c1591eab095b1f2a9863de4c
Shreyash <shreyashdesai31796@gmail.com>
2024-11-13 23:59:49 +0530

    first

---
+++ conditions.py
@@ -0,0 +1,20 @@
+print("mon\ntue\nwed\nthur\nfri\nsat\nsun")
+
+day = input("ENTER DAY :")
+
+if day == "mon":  
+    print("It's",day)
+elif day == "tue":  
+    print("It's",day)
+elif day == "wed":  
+    print("It's",day)
+elif day == "thur":  
+    print("It's",day)
+elif day == "fri":  
+    print("It's",day)
+elif day == "sat":  
+    print("It's",day)
+elif day == "sun":  
+    print("It's",day)
+else:
+    print("ERROR")
