31fb0008747faebb03bb90e5be4ff62f2b9e6c86
Shreyash <shreyashdesai31796@gmail.com>
2024-12-23 22:56:52 +0530

    2024-12-23-22-56-52

---
+++ udemy/3_Lists/lists.py
@@ -0,0 +1,39 @@
+#Collection of same data types
+
+#LIST
+# print("--------LIST-----------")
+# get = int(input("ENTER LIST NO. YOU WANT TO ENTER: "))
+# l = []
+# for i in range(0,get):
+#     a = input(f"{i} = ")
+#     l.append(a)
+
+# print(l)
+
+#ADDING LIST IN LIST
+
+# print("\nNOW LETS ENTER DIFF DATA TYPES IN ONE LIST")
+# print("--------ADDING LIST IN LIST-----------")
+# get2 = int(input("\nENTER HOW MANY LISTS IN LIST YOU NEED: "))
+# l2 = []
+# l3 = []
+# for i in range (0,get2):
+#     get3 = int(input(f"\n{i} LIST LENGTH:"))
+#     # typee = input("ENTER TYPE OF LIST: ") 
+#     for j in range (0,get3):
+#         # val = input()
+#         # if val == typee:
+#         get4 = input()
+#         l3.append(get4)
+#     l2.append(l3) 
+        
+# print(l2)
+
+#DELETING LIST
+# print("--------DELETING ITEM OF LIST-----------")
+# spam = [1,2,3,4,5,6]
+# print(spam)
+# delete = int(input("DELETE:"))
+# del spam [delete]
+# print(spam)
+
--- udemy/try.py
+++ udemy/try.py
@@ -7,6 +7,42 @@
 #     print("ERROR")
     
 
-name = "asdd,asdwer,awfrh,xerg"
-comma = name.count(",")
-print(comma)
+# name = "asdd,asdwer,awfrh,xerg"
+# x = ""
+# end = []
+# c_count = 0
+# comma = name.count(",")
+# for i in name:
+#     if i != ",":
+#         name = name + i
+#     elif i == ",":
+#         c_count = +1
+#         end.append(name)
+#     elif c_count == comma:
+#         break
+# print(comma)
+# print(end[0:1])
+
+# name = 'SHREYASH'
+# print(name)
+# print(len(name))
+# x = len(name)-1
+# print(x)
+
+# print(name[1:x])
+
+# def remove_char(s):
+#     x = len(s)-1
+#     print(s[1:x])
+        
+# name = input("ENTER NAME TO BE SPLITTED:")
+# remove_char(name)
+
+arr = [1,2,3,4,5,6,7]
+print(arr)
+for i in arr:
+    j = len(arr)
+    for j in range (len(arr),i):
+        print("i",i)
+        print("j",j)
+    print("------------")
